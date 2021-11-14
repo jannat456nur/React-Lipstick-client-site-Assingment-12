21cc25e499ede74f4d119ec44945e673adce873c
jannat456nur <jannatjenny22@gmail.com>
2021-11-14 05:19:49 -0800

    oise

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -15,6 +15,7 @@ const Login = () => {
     const location = useLocation();
     const history = useHistory();
 //button control functionalities
+// create functionalities
     const handleOnChange = e => {
         const field = e.target.name;
         const value = e.target.value;
